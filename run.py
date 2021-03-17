2ef7e96e0aa30c1ed8162e82cd7bd2828308badd
Tom <44496697+t1goe@users.noreply.github.com>
2021-03-17 18:10:02 +0000

    Finished preprocessing

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import math
 import os
 import time
+from os.path import isfile, join
 
 import data_preprocessing
 
@@ -30,28 +31,44 @@ def station_epoch_time(x):
     return int(time.mktime(time.strptime(temp_x, "%Y-%m-%d %H")))
 
 
-def attach_weather_data(station_file):
-    station_dataframe = pd.read_csv(station_file)
+def attach_weather_data(path, station_file):
+    if not os.path.exists(path + station_file):
+        print("File: " + str(path + station_file) + " does not exist")
+        return
+
+    station_dataframe = pd.read_csv(path + station_file)
 
     if 'TIME' not in station_dataframe.columns:
         print('TIME column not present')
         return
 
-    if not os.path.exists('datasets/bss/dublin/reorg_plus_weather'):
-        os.makedirs('datasets/bss/dublin/reorg_plus_weather')
+    dest = './datasets/bss/dublin/reorg_plus_weather/'
+
+    if not os.path.exists(dest):
+        os.makedirs(dest)
+
+    if os.path.exists(dest + station_file):
+        # print("File plus weather already exists")
+        return
 
     weather = pd.read_csv('datasets/weather/hly175/hly175clean.csv')
 
     new_weather_dataframe = pd.DataFrame(np.repeat(weather.values, 12, axis=0), columns=weather.columns)
 
     result = pd.concat([station_dataframe, new_weather_dataframe], axis=1)
-    result.to_csv(r'datasets/bss/dublin/reorg_plus_weather/station_2.csv', index=False)
+    result.to_csv('datasets/bss/dublin/reorg_plus_weather/' + station_file, index=False)
 
 
-attach_weather_data('datasets/bss/dublin/reorg/station_2.csv')
+# attach_weather_data('datasets/bss/dublin/reorg/station_2.csv')
+# mypath = "./datasets/bss/dublin/reorg/"
+#
+# files = [f for f in os.listdir(mypath) if isfile(join(mypath, f))]
+#
+# for file in tqdm(files):
+#     # print(file)
+#     attach_weather_data(mypath, file)
 
-# data_preprocessing.clean_weather_data()
-
+data_preprocessing.organise_by_station()
 # dataset = pd.read_csv('datasets/bss/dublin/reorg/station_2.csv', usecols=['TIME', 'AVAILABLE BIKES'])
 # dataset.columns = ['TIME', 'BIKES']
 
