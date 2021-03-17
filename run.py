b077a7e8fbd2689a0ee564018824cd78cbfe0abf
Tom <44496697+t1goe@users.noreply.github.com>
2021-03-17 16:52:19 +0000

    Added ability to add weather data to station data

--- run.py
+++ run.py
@@ -2,37 +2,65 @@ import math
 import os
 import time
 
+import data_preprocessing
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import data_preprocessing
 import pandas as pd
 import numpy as np
+from tqdm import tqdm
 
 from calendar import timegm
 
 
-def attach_weather_data(x):
-    if 'TIME' not in x.columns:
+def station_get_weather(x):
+    time_sec = int(time.mktime(time.strptime("2018-08-01 12", "%Y-%m-%d %H")))
+
+    print(time_sec)
+
+
+def get_weather_at_time(t, data):
+    x = data[data['epoch'] == t]
+    return x
+
+
+def station_epoch_time(x):
+    temp_x = x[:-6]
+    return int(time.mktime(time.strptime(temp_x, "%Y-%m-%d %H")))
+
+
+def attach_weather_data(station_file):
+    station_dataframe = pd.read_csv(station_file)
+
+    if 'TIME' not in station_dataframe.columns:
         print('TIME column not present')
         return
 
+    if not os.path.exists('datasets/bss/dublin/reorg_plus_weather'):
+        os.makedirs('datasets/bss/dublin/reorg_plus_weather')
+
     weather = pd.read_csv('datasets/weather/hly175/hly175clean.csv')
 
-    utc_time = time.strptime("08-aug-2018 03:00", "%d-%b-%Y %H:%M")
-    print(utc_time)
-    # epoch_time = timegm(utc_time)
-    # -> 1236472051
+    new_weather_dataframe = pd.DataFrame(np.repeat(weather.values, 12, axis=0), columns=weather.columns)
+
+    result = pd.concat([station_dataframe, new_weather_dataframe], axis=1)
+    result.to_csv(r'datasets/bss/dublin/reorg_plus_weather/station_2.csv', index=False)
 
 
-dataset = pd.read_csv('datasets/bss/dublin/reorg/station_2.csv', usecols=['TIME', 'AVAILABLE BIKES'])
-dataset.columns = ['TIME', 'BIKES']
+attach_weather_data('datasets/bss/dublin/reorg/station_2.csv')
+
+# data_preprocessing.clean_weather_data()
+
+# dataset = pd.read_csv('datasets/bss/dublin/reorg/station_2.csv', usecols=['TIME', 'AVAILABLE BIKES'])
+# dataset.columns = ['TIME', 'BIKES']
 
 # dataset['bikes(t-1)'] = dataset['bikes'].shift(1)
 
 # attach_weather_data(dataset)
 
-utc_time = time.strptime("08-aug-2018 03:00", "%d-%b-%Y %H:%M")
-# utc_time.
-print(utc_time)
+# utc_time = time.mktime(time.strptime("08-aug-2018 03:00", "%d-%b-%Y %H:%M"))
+
+# print(utc_time)
 
 # print(dataset)
