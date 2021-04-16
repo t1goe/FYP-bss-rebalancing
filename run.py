7c7c727c0437bfb619a7d1eee437008a4092ae4f
Tom <44496697+t1goe@users.noreply.github.com>
2021-04-16 12:58:12 +0100

    updated notebooks

--- run.py
+++ run.py
@@ -1,83 +1,124 @@
-import math
-import os
-import time
-from os.path import isfile, join
-
-import data_preprocessing
-
-import pandas as pd
-import matplotlib.pyplot as plt
-import data_preprocessing
-import pandas as pd
+import sys
 import numpy as np
-from tqdm import tqdm
+from math import sqrt
+from numpy import concatenate
+from matplotlib import pyplot
+from pandas import read_csv
+from pandas import DataFrame
+from pandas import concat
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
 
-from calendar import timegm
+# convert series to supervised learning
+# def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
+#     n_vars = 1 if type(data) is list else data.shape[1]
+#     df = DataFrame(data)
+#     cols, names = list(), list()
+#     # input sequence (t-n, ... t-1)
+#     for i in range(n_in, 0, -1):
+#         cols.append(df.shift(i))
+#         names += [('var%d(t-%d)' % (j + 1, i)) for j in range(n_vars)]
+#     # forecast sequence (t, t+1, ... t+n)
+#     for i in range(0, n_out):
+#         cols.append(df.shift(-i))
+#         if i == 0:
+#             names += [('var%d(t)' % (j + 1)) for j in range(n_vars)]
+#         else:
+#             names += [('var%d(t+%d)' % (j + 1, i)) for j in range(n_vars)]
+#     # put it all together
+#     agg = concat(cols, axis=1)
+#     agg.columns = names
+#     # drop rows with NaN values
+#     if dropnan:
+#         agg.dropna(inplace=True)
+#     return agg
 
+# load dataset
+dataset = read_csv('datasets/bss/dublin/reorg/station_2.csv')
 
-def station_get_weather(x):
-    time_sec = int(time.mktime(time.strptime("2018-08-01 12", "%Y-%m-%d %H")))
+dataset = dataset.drop('TIME', axis=1)
+# print(dataset.head())
+values = dataset.values
 
-    print(time_sec)
+# ensure all data is float
+values = values.astype('float32')
+# normalize features
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled = scaler.fit_transform(values)
+# frame as supervised learning
+reframed = scaled
 
+print(scaled)
 
-def get_weather_at_time(t, data):
-    x = data[data['epoch'] == t]
-    return x
+# split into train and test sets
+# values = reframed.values
 
+train_start = 0
+train_end = 8760
 
-def station_epoch_time(x):
-    temp_x = x[:-6]
-    return int(time.mktime(time.strptime(temp_x, "%Y-%m-%d %H")))
+test_start = 99144
+test_end = 108071
 
+n_train_hours = 365 * 24
+train = scaled[train_start:train_end, :]
+test = scaled[test_start:test_end, :]
+# train = values[train_start:train_end, :]
+# test = values[test_start:test_end, :]
 
-def attach_weather_data(path, station_file):
-    if not os.path.exists(path + station_file):
-        print("File: " + str(path + station_file) + " does not exist")
-        return
+# split into input and outputs
+train_X, train_y = train[:, 1:], train[:, 1]
+test_X, test_y = test[:, 1:], test[:, 1]
+# reshape input to be 3D [samples, timesteps, features]
+train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
+test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
+print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
 
-    station_dataframe = pd.read_csv(path + station_file)
+# design network
+model = Sequential()
+model.add(LSTM(50, input_shape=(train_X.shape[1], train_X.shape[2])))
+model.add(Dense(1))
+model.compile(loss='mae', optimizer='adam')
+# fit network
+history = model.fit(train_X, train_y, epochs=150, batch_size=72, validation_data=(test_X, test_y), verbose=1,
+                    shuffle=False)
+# plot history
+pyplot.plot(history.history['loss'], label='train')
+pyplot.plot(history.history['val_loss'], label='test')
+pyplot.legend()
+pyplot.show()
 
-    if 'TIME' not in station_dataframe.columns:
-        print('TIME column not present')
-        return
+# make a prediction
+yhat = model.predict(test_X)
+# print(test_X)
+# print(yhat)
+test_X = test_X.reshape((test_X.shape[0], test_X.shape[2]))
+# invert scaling for forecast
+inv_yhat = concatenate((yhat, test_X), axis=1)
+inv_yhat = scaler.inverse_transform(inv_yhat)
+inv_yhat = inv_yhat[:, 0]
+# invert scaling for actual
+test_y = test_y.reshape((len(test_y), 1))
+inv_y = concatenate((test_y, test_X), axis=1)
+inv_y = scaler.inverse_transform(inv_y)
+inv_y = inv_y[:, 0]
+# calculate RMSE
 
-    dest = './datasets/bss/dublin/reorg_plus_weather/'
+# np.set_printoptions(threshold=sys.maxsize)
+# temp = concatenate((inv_y, inv_yhat))
+# print(temp)
+# print(inv_y)
+# print(inv_yhat)
 
-    if not os.path.exists(dest):
-        os.makedirs(dest)
-
-    if os.path.exists(dest + station_file):
-        # print("File plus weather already exists")
-        return
-
-    weather = pd.read_csv('datasets/weather/hly175/hly175clean.csv')
-
-    new_weather_dataframe = pd.DataFrame(np.repeat(weather.values, 12, axis=0), columns=weather.columns)
-
-    result = pd.concat([station_dataframe, new_weather_dataframe], axis=1)
-    result.to_csv('datasets/bss/dublin/reorg_plus_weather/' + station_file, index=False)
-
-
-# attach_weather_data('datasets/bss/dublin/reorg/station_2.csv')
-# mypath = "./datasets/bss/dublin/reorg/"
-#
-# files = [f for f in os.listdir(mypath) if isfile(join(mypath, f))]
-#
-# for file in tqdm(files):
-#     # print(file)
-#     attach_weather_data(mypath, file)
-
-data_preprocessing.organise_by_station()
-# dataset = pd.read_csv('datasets/bss/dublin/reorg/station_2.csv', usecols=['TIME', 'AVAILABLE BIKES'])
-# dataset.columns = ['TIME', 'BIKES']
-
-# dataset['bikes(t-1)'] = dataset['bikes'].shift(1)
-
-# attach_weather_data(dataset)
-
-# utc_time = time.mktime(time.strptime("08-aug-2018 03:00", "%d-%b-%Y %H:%M"))
-
-# print(utc_time)
-
-# print(dataset)
+print()
+mae = mean_absolute_error(inv_y, inv_yhat)
+mse = mean_squared_error(inv_y, inv_yhat)
+rmse = sqrt(mean_squared_error(inv_y, inv_yhat))
+print('Test MAE: %.3f' % mae)
+print('Test MSE: %.3f' % mse)
+print('Test RMSE: %.3f' % rmse)
