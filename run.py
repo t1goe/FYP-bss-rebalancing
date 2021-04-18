d73913cd6afcccdb0601a067e6b6ca858109c4df
Tom <44496697+t1goe@users.noreply.github.com>
2021-04-18 13:53:27 +0100

    Experimenting with python frontend

--- run.py
+++ run.py
@@ -1,124 +1,71 @@
-import sys
-import numpy as np
-from math import sqrt
-from numpy import concatenate
-from matplotlib import pyplot
-from pandas import read_csv
-from pandas import DataFrame
-from pandas import concat
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import mean_squared_error
-from sklearn.metrics import mean_absolute_error
-from sklearn.metrics import mean_squared_error
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
+# img_viewer.py
 
-# convert series to supervised learning
-# def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
-#     n_vars = 1 if type(data) is list else data.shape[1]
-#     df = DataFrame(data)
-#     cols, names = list(), list()
-#     # input sequence (t-n, ... t-1)
-#     for i in range(n_in, 0, -1):
-#         cols.append(df.shift(i))
-#         names += [('var%d(t-%d)' % (j + 1, i)) for j in range(n_vars)]
-#     # forecast sequence (t, t+1, ... t+n)
-#     for i in range(0, n_out):
-#         cols.append(df.shift(-i))
-#         if i == 0:
-#             names += [('var%d(t)' % (j + 1)) for j in range(n_vars)]
-#         else:
-#             names += [('var%d(t+%d)' % (j + 1, i)) for j in range(n_vars)]
-#     # put it all together
-#     agg = concat(cols, axis=1)
-#     agg.columns = names
-#     # drop rows with NaN values
-#     if dropnan:
-#         agg.dropna(inplace=True)
-#     return agg
+import PySimpleGUI as sg
+import os.path
 
-# load dataset
-dataset = read_csv('datasets/bss/dublin/reorg/station_2.csv')
+# First the window layout in 2 columns
 
-dataset = dataset.drop('TIME', axis=1)
-# print(dataset.head())
-values = dataset.values
+file_list_column = [
+    [
+        sg.Text("Image Folder"),
+        sg.In(size=(25, 1), enable_events=True, key="-FOLDER-"),
+        sg.FolderBrowse(),
+    ],
+    [
+        sg.Listbox(
+            values=[], enable_events=True, size=(40, 20), key="-FILE LIST-"
+        )
+    ],
+]
 
-# ensure all data is float
-values = values.astype('float32')
-# normalize features
-scaler = MinMaxScaler(feature_range=(0, 1))
-scaled = scaler.fit_transform(values)
-# frame as supervised learning
-reframed = scaled
+# For now will only show the name of the file that was chosen
+image_viewer_column = [
+    [sg.Text("Choose an image from list on left:")],
+    [sg.Text(size=(40, 1), key="-TOUT-")],
+    [sg.Image(key="-IMAGE-")],
+]
 
-print(scaled)
+# ----- Full layout -----
+layout = [
+    [
+        sg.Column(file_list_column),
+        sg.VSeperator(),
+        sg.Column(image_viewer_column),
+    ]
+]
 
-# split into train and test sets
-# values = reframed.values
+window = sg.Window("Image Viewer", layout)
 
-train_start = 0
-train_end = 8760
+while True:
+    event, values = window.read()
+    if event == "Exit" or event == sg.WIN_CLOSED:
+        break
 
-test_start = 99144
-test_end = 108071
+    # Folder name was filled in, make a list of files in the folder
+    if event == "-FOLDER-":
+        folder = values["-FOLDER-"]
+        try:
+            # Get list of files in folder
+            file_list = os.listdir(folder)
+        except:
+            file_list = []
 
-n_train_hours = 365 * 24
-train = scaled[train_start:train_end, :]
-test = scaled[test_start:test_end, :]
-# train = values[train_start:train_end, :]
-# test = values[test_start:test_end, :]
+        fnames = [
+            f
+            for f in file_list
+            if os.path.isfile(os.path.join(folder, f))
+               and f.lower().endswith((".png", ".gif"))
+        ]
+        window["-FILE LIST-"].update(fnames)
+    elif event == "-FILE LIST-":  # A file was chosen from the listbox
+        try:
+            filename = os.path.join(
+                values["-FOLDER-"], values["-FILE LIST-"][0]
+            )
+            window["-TOUT-"].update(filename)
+            window["-IMAGE-"].update(filename=filename)
+        except:
+            pass
 
-# split into input and outputs
-train_X, train_y = train[:, 1:], train[:, 1]
-test_X, test_y = test[:, 1:], test[:, 1]
-# reshape input to be 3D [samples, timesteps, features]
-train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
-test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
-print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
 
-# design network
-model = Sequential()
-model.add(LSTM(50, input_shape=(train_X.shape[1], train_X.shape[2])))
-model.add(Dense(1))
-model.compile(loss='mae', optimizer='adam')
-# fit network
-history = model.fit(train_X, train_y, epochs=150, batch_size=72, validation_data=(test_X, test_y), verbose=1,
-                    shuffle=False)
-# plot history
-pyplot.plot(history.history['loss'], label='train')
-pyplot.plot(history.history['val_loss'], label='test')
-pyplot.legend()
-pyplot.show()
-
-# make a prediction
-yhat = model.predict(test_X)
-# print(test_X)
-# print(yhat)
-test_X = test_X.reshape((test_X.shape[0], test_X.shape[2]))
-# invert scaling for forecast
-inv_yhat = concatenate((yhat, test_X), axis=1)
-inv_yhat = scaler.inverse_transform(inv_yhat)
-inv_yhat = inv_yhat[:, 0]
-# invert scaling for actual
-test_y = test_y.reshape((len(test_y), 1))
-inv_y = concatenate((test_y, test_X), axis=1)
-inv_y = scaler.inverse_transform(inv_y)
-inv_y = inv_y[:, 0]
-# calculate RMSE
-
-# np.set_printoptions(threshold=sys.maxsize)
-# temp = concatenate((inv_y, inv_yhat))
-# print(temp)
-# print(inv_y)
-# print(inv_yhat)
-
-print()
-mae = mean_absolute_error(inv_y, inv_yhat)
-mse = mean_squared_error(inv_y, inv_yhat)
-rmse = sqrt(mean_squared_error(inv_y, inv_yhat))
-print('Test MAE: %.3f' % mae)
-print('Test MSE: %.3f' % mse)
-print('Test RMSE: %.3f' % rmse)
+window.close()
